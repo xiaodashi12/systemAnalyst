53ef4ed259266facc7fe69662449b172a6f75ee0
xiaodashi12 <1207688613@qq.com>
2026-06-04 10:11:51 +0800

    fix: switch to hash router and relative base URL for universal deployment compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
@@ -15,7 +15,8 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
+
 
